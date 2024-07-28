cd92503cab5fba9b8181609db1d6f7cc414f443f
China-quanda <864910436@qq.com>
2024-07-29 00:56:41 +0800

    edit Prisma schema file

---
+++ prisma/migrations/20240728165313_init/migration.sql
@@ -0,0 +1,123 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "phone" INTEGER NOT NULL,
+    "name" TEXT,
+    "sex" TEXT NOT NULL,
+    "birthday" TIMESTAMP(3) NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Address" (
+    "id" SERIAL NOT NULL,
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_time" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "sex" TEXT NOT NULL,
+    "phone" INTEGER NOT NULL,
+    "is_default" BOOLEAN NOT NULL DEFAULT false,
+    "latitude" TEXT NOT NULL,
+    "longitude" TEXT NOT NULL,
+    "country" TEXT NOT NULL DEFAULT '中国',
+    "province" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "district" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "streetNum" TEXT NOT NULL,
+    "cityCode" TEXT NOT NULL,
+    "userId" INTEGER,
+
+    CONSTRAINT "Address_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Shop" (
+    "id" SERIAL NOT NULL,
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_time" TIMESTAMP(3) NOT NULL,
+    "no" INTEGER NOT NULL,
+    "logo" TEXT NOT NULL,
+    "latitude" TEXT NOT NULL,
+    "longitude" TEXT NOT NULL,
+    "country" TEXT NOT NULL DEFAULT '中国',
+    "province" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "district" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "streetNum" TEXT NOT NULL,
+    "cityCode" TEXT NOT NULL,
+
+    CONSTRAINT "Shop_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" SERIAL NOT NULL,
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_time" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "sex" TEXT NOT NULL,
+    "phone" INTEGER NOT NULL,
+    "birthday" TIMESTAMP(3) NOT NULL,
+    "marital_status" TIMESTAMP(3) NOT NULL,
+    "entry_date" TIMESTAMP(3) NOT NULL,
+    "shopId" INTEGER,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Table" (
+    "id" SERIAL NOT NULL,
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_time" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "no" TEXT NOT NULL,
+    "qr_code" TEXT NOT NULL,
+    "shopId" INTEGER,
+
+    CONSTRAINT "Table_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_time" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "shopId" INTEGER,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_time" TIMESTAMP(3) NOT NULL,
+    "name" TEXT NOT NULL,
+    "categoryId" INTEGER,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
+-- AddForeignKey
+ALTER TABLE "Address" ADD CONSTRAINT "Address_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Employee" ADD CONSTRAINT "Employee_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Table" ADD CONSTRAINT "Table_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Category" ADD CONSTRAINT "Category_shopId_fkey" FOREIGN KEY ("shopId") REFERENCES "Shop"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
