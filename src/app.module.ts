3a23c50a360bbcb4357e6ea99a681e1af4fb6e94
China-quanda <864910436@qq.com>
2024-07-29 09:07:35 +0800

    modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AddressModule } from './address/address.module';
-import { CategoryModule } from './category/category.module';
-import { ShopModule } from './shop/shop.module';
-import { OrderModule } from './order/order.module';
-import { ProductModule } from './product/product.module';
-import { EmployeeModule } from './employee/employee.module';
-import { RoleModule } from './role/role.module';
-import { TableModule } from './table/table.module';
-import { IntegralModule } from './integral/integral.module';
-import { CouponModule } from './coupon/coupon.module';
+import { UserModule } from './modules/user/user.module';
+import { AddressModule } from './modules/address/address.module';
+import { CategoryModule } from './modules/category/category.module';
+import { ShopModule } from './modules/shop/shop.module';
+import { OrderModule } from './modules/order/order.module';
+import { ProductModule } from './modules/product/product.module';
+import { EmployeeModule } from './modules/employee/employee.module';
+import { RoleModule } from './modules/role/role.module';
+import { TableModule } from './modules/table/table.module';
+import { IntegralModule } from './modules/integral/integral.module';
+import { CouponModule } from './modules/coupon/coupon.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, AddressModule, CategoryModule, ShopModule, OrderModule, ProductModule, EmployeeModule, RoleModule, TableModule, IntegralModule, CouponModule, PrismaModule],
+  imports: [
+    UserModule,
+    AddressModule,
+    CategoryModule,
+    ShopModule,
+    OrderModule,
+    ProductModule,
+    EmployeeModule,
+    RoleModule,
+    TableModule,
+    IntegralModule,
+    CouponModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
